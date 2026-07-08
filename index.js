df755a6f8b43c8cc7fe3d2369abf6ede0374ce11
Greg Telakis <greg.telakis@gmail.com>
2026-07-08 12:29:20 +0200

    Finalized Step.2 Create Card Component via JS.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createCharacterCard } from "./components/CharacterCard/CharacterCard.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,10 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+
+// create variable that captures card creation
+const createCard = createCharacterCard();
+
+// append card creation
+cardContainer.append(createCard);
